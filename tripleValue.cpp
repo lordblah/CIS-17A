86326376416754eb311e27e42add8d9e12da1610
Jonathan Hernandez <helharatheband@gmail.com>
2018-05-07 16:34:37 -0700

    Create tripleValue.cpp
    started off as doubling the value and changed it to tripling.

---
+++ tripleValue.cpp
@@ -0,0 +1,47 @@
+// This program uses two functions that accept addresses of
+// variables as arguments.
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+  // Function prototypes
+  void getNumber(int *);
+  void tripleValue(int *);
+ 
+  int main()
+  {
+    int number;
+
+    // Call getNumber and pass the address of number.
+    getNumber(&number);
+
+    // Call tripleValue and pass the address of number.
+	tripleValue(&number);
+
+    // Display the value in number.
+    cout << "That value tripled is " << number << endl;
+    return 0;
+  }
+
+    //****************************************************************
+    // Definition of getNumber. The parameter, input, is a pointer.  *
+    // This function asks the user for a number. The value entered   *
+    // is stored in the variable pointed to by input.                *
+    //****************************************************************
+    
+    void getNumber(int *input)
+    {
+        cout << "Enter an integer number: ";
+        cin >> *input;
+    }
+    
+    //****************************************************************
+    // Definition of tripleValue. The parameter, val, is a pointer.  *
+    // This function multiplies the variable pointed to by val by    *
+    // two.                                                          *
+    //****************************************************************
+
+    void tripleValue(int *val)
+    {
+      *val = pow(*val,3);
+    }
